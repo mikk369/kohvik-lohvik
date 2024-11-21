4befafb22f371c50520e397a8d8bdc15b34faeba
Mike <jogdial25@gmail.com>
2024-11-21 23:29:27 +0200

    added about section, menu modal

--- src/App.jsx
+++ src/App.jsx
@@ -60,6 +60,32 @@ function App() {
             }
           );
           obs.observe(landingSection);
+
+          //MODAL\\
+          const menuLink = document.getElementById('menu-link');
+          const modal = document.getElementById('menu-modal');
+          const closeButton = modal.querySelector('.close');
+
+          // Open modal when clicking the Menüü link
+          menuLink.addEventListener('click', (e) => {
+            e.preventDefault();
+            modal.style.display = 'block';
+            document.body.classList.add("no-scroll");
+          });
+
+          // Close modal when clicking the close button
+          closeButton.addEventListener('click', () => {
+            modal.style.display = 'none';
+            document.body.classList.remove("no-scroll");
+          });
+
+          // Close modal when clicking outside the modal content
+          window.addEventListener('click', (e) => {
+            if (e.target === modal) {
+              modal.style.display = 'none';
+              document.body.classList.remove("no-scroll");
+            }
+          });
     }, []);
 
   return (
@@ -69,13 +95,15 @@ function App() {
           <img src="lohviklogo.png" alt="site logo" className="logo" />
         </a>
         <div className="nav-wrapper">
-          <nav className="main-nav">
+        <nav className="main-nav">
             <ul className="main-nav-list">
-              <li><a href="" className="main-nav-link">Esileht</a></li>
-              <li><a href="" className="main-nav-link">Menüü</a></li>
+              <li><a href="#landing" className="main-nav-link">Esileht</a></li>
+              <li><a href="#about" className="main-nav-link">Meie lugu</a></li>
+              <li><a href="#menu" className="main-nav-link" id="menu-link">Menüü</a></li>
               <h2 className="heading-secondary">KOHVIK LOHVIK</h2>
-              <li><a href="" className="main-nav-link">Meie lugu</a></li>
-              <li><a href="" className="main-nav-link">Väärtused</a></li>
+              <li><a href="#fb-posts" className="main-nav-link">Postitused</a></li>
+              <li><a href="#supporters" className="main-nav-link">Toetajad</a></li>
+              <li><a href="#our-values" className="main-nav-link">Väärtused</a></li>
             </ul>
           </nav>
           <button className="btn-mobile-nav" onClick={toggleNav}>
@@ -84,15 +112,73 @@ function App() {
         </div>
       </header>
       <main>
-        <section className="landing-section">
+        <section className="landing-section" id="landing">
           <div className="landing">
-            <h1 className="heading-primary">Lohviku kogemusõppe akadeemia</h1>
+            <h1 className="heading-primary">Lohviku <span className='headingHighlight'>kogemusõppe</span> akadeemia</h1>
+          </div>
+        </section>
+        <section className='about-us-wrapper' id="about">
+          <div className="about-us">
+            <div className="about-header">
+              <h3 className="heading-tertiary">Meie lugu</h3>
+            </div>
+            <div className="about-grid">
+              <div className="about-text-box">
+                <h3 className='about-title'>Kuidas kõik algas?</h3>
+                <p className='about-description'>
+                MTÜ Teeklubi alustas Elvas raamatulugemise ja teejoomise kultuuri arendamisest, kuid koroonaajal 
+                sündis Lohvik – laste oma kohvik. 2020. aasta lõpus loodi koos Elva Vallavalitsusega noorte kogemus - ja äriõppe labor. 
+                Nüüd keskendub ühing noorte kaasamisele ja mentorprogrammide kaudu oskuste praktiseerimisele päris ettevõttes.
+                </p>
+              </div>
+              <div className="about-icon-box">
+                <span className='about-icon'><i className="fa-regular fa-lightbulb lightbulb-icon"></i></span>
+              </div>
+              <div className="about-icon-box">
+                <span className='about-icon'><i className="fa-solid fa-question question-icon"></i></span>
+              </div>
+              <div className="about-text-box">
+                <h3 className='about-title'>Millega on tegemist?</h3>
+                <p className='about-description'>
+                Lohvik Kogemusõppe Akadeemia on koolinoortele suunatud unikaalne ärilabor. Programmis õpivad noored ettevõtte loomise ja juhtimise oskusi, luues oma kohviku. 
+                Suvehooajal tegutsevad nad toiduhaagisega, tehes ise kõike alates menüüst ja kauba tellimisest kuni toiduvalmistamise ja müügitulemuste analüüsini. 
+                Programmi eripära on praktilise kogemuse ja ettevõtlusteadmiste ühendamine.
+                </p>
+              </div>
+              <div className="about-text-box">
+                <h3 className='about-title'>Kuidas seni on läinud?</h3>
+                <p className='about-description'>
+                Noortele suunatud kogemusõppe programm Lohvik – laste oma kohvik, sai alguse 2020. aasta lõpus. Kolme aasta jooksul on programm toonud Elvas Arbi järve äärde noorte juhitud kohviku. 
+                2024. aastal muutub Lohvik mobiilseks toiduhaagiseks, jõudes sinna, meid kõige enam oodatakse.
+                </p>
+              </div>
+              <div className="about-icon-box">
+                <span className='about-icon'><i className="fa-solid fa-chart-line chart-icon"></i></span>
+              </div>
+              <div className="about-icon-box">
+                <span className='about-icon'><i className="fa-solid fa-bullseye bullseye-icon"></i></span>
+              </div>
+              <div className="about-text-box">
+                <h3 className='about-title'>Tegevuse eesmärk</h3>
+                <p className='about-description'>
+                Teeklubi MTÜ eesmärk on tõsta teadlikkust noorte kaasamise ja kogemusõppe vajalikkusest, edendada vaimset tervist, sotsiaalseid oskusi ja vaba aja sisustamist. 
+                Toetame individuaalset haridust, kasutades uuenduslikke tehnoloogiaid nagu AI. Oleme usaldusväärne partner koolidele ja omavalitsustele, aidates süsteemselt ennetada 
+                kogemusõppe probleeme ja suurendada mõju.
+                </p>
+              </div>
+            </div>
           </div>
         </section>
         <section className="section-hero">
           <div className="hero">
-            <div className="posts">
-            <h2 className="heading-tertiary">Postitused</h2>
+            <div id="menu-modal" className="modal">
+              <div className="modal-content">
+                <span className="close" title='Close'><i class="fa fa-times"></i></span>
+                <img src="./menuimg.png" alt="Lohiku Kohviku Menüü" className="menuimg"/>
+              </div>
+            </div>
+            <section className="posts" id="fb-posts">
+            <h3 className="heading-tertiary">Postitused</h3>
               {loading ? (
                  <div className="loading-spinner"></div>
               ) : posts.length > 0 ? (
@@ -124,9 +210,9 @@ function App() {
               ) : (
                 <p className='no-posts'>No posts to display</p>
               )}
-            </div>
-            <div className="section-supporters">
-              <h2 className='heading-tertiary'>Lohvik toetajad ja koostööpartnerid läbi aastate</h2>
+            </section>
+            <section className="section-supporters" id="supporters">
+              <h3 className='heading-tertiary'>Lohvik toetajad ja koostööpartnerid läbi aastate</h3>
                 <div className="supporter-logos">
                   <img src="kohalik.png" alt="kohalik logo" className="supporter-image" />
                   <img src="noa.png" alt="nõa logo" className="supporter-image" />
@@ -147,7 +233,10 @@ function App() {
                   <p>KOP Kohalik Omaalgatuse Programm</p>
                   <p>LEADER programm</p>
                 </div>
-            </div>
+            </section>
+            <section className="our-values" id="our-values">
+              <h3 className="heading-tertiary">Väärtused</h3>
+            </section>
             <div className="footer">
               <div className="footer-content">
                 <p className="contacts">
